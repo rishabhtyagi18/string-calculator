bff8bac57cd6853f2b1f5fce8d93171183902e29
rishabhtyagi18 <rishabhtyagi18@gmail.com>
2025-03-27 23:14:18 +0530

    Implement add function to throw error for negative numbers

--- src/test/StringCalculator.test.js
+++ src/test/StringCalculator.test.js
@@ -26,4 +26,9 @@ describe("String Calculator", () => {
     expect(add("//;\n1;2")).toBe(3);
     expect(add("//|\n3|4|5")).toBe(12);
   });
+
+  test("throws an error for negative numbers", () => {
+    expect(() => add("1,-2,3,-4")).toThrow("Negative numbers not allowed -2,-4");
+    expect(() => add("1,-2,-3, 4")).toThrow("Negative numbers not allowed -2,-3");
+  });
 });
