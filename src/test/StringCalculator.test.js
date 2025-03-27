b004376282876df1b564fffe9ec6f5c67ae6c88a
rishabhtyagi18 <rishabhtyagi18@gmail.com>
2025-03-27 23:08:56 +0530

    supports new lines as delimiters

--- src/test/StringCalculator.test.js
+++ src/test/StringCalculator.test.js
@@ -17,4 +17,8 @@ describe("String Calculator", () => {
   test("handles an unknown amount of numbers", () => {
     expect(add("1,2,3,4,5")).toBe(15);
   });
+
+  test("supports new lines as delimiters", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
