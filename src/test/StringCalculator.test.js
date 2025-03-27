4c51241924a09099e8f3e072a47ff41101c38e05
rishabhtyagi18 <rishabhtyagi18@gmail.com>
2025-03-27 23:11:16 +0530

    handle custom delimiters

--- src/test/StringCalculator.test.js
+++ src/test/StringCalculator.test.js
@@ -21,4 +21,9 @@ describe("String Calculator", () => {
   test("supports new lines as delimiters", () => {
     expect(add("1\n2,3")).toBe(6);
   });
+
+  test("supports custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//|\n3|4|5")).toBe(12);
+  });
 });
